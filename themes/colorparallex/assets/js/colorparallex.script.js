799f8e7acc2369ea7f8c4468dddb1c1d9a6034fb
John <lijd@eabax.com>
2018-07-21 08:28:03 +0800

    fix colorparallex js.

--- themes/colorparallex/assets/js/colorparallex.script.js
+++ themes/colorparallex/assets/js/colorparallex.script.js
@@ -2,19 +2,32 @@
  * @file
  * Custom scripts for theme.
  */
-(function ($) {
+(function ($, Drupal) {
 
   Drupal.behaviors.colorParallex = {
-    attach: function (context) {
+    attach: function attach(context) {
       // 01. Handle Home Content Height
       $('#home').height($(window).height());
-
       $(window).on('resize', function () {
         $('#home').height($(window).height());
       });
+
+      // 02. Handle Header Navigation State
+      // $(window).on('scroll load', function () {
+      //   if ($('#header').attr('data-state-change') != 'disabled') {
+      //     var totalScroll = $(window).scrollTop();
+      //     var headerHeight = $('#header').height();
+      //
+      //     if (totalScroll >= headerHeight) {
+      //       $('#header').addClass('navbar-small');
+      //     } else {
+      //       $('#header').removeClass('navbar-small');
+      //     }
+      //   }
+      // });
 
       Drupal.attachBehaviors(context);
     }
   }
 
-})(jQuery);
+})(jQuery, Drupal);
