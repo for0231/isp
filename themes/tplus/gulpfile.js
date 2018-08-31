9c79513ec8c8a73ee598376a41624bcb16cc3494
John <John-lee@2001594.no-reply.drupal.org>
2018-08-31 23:13:20 +0800

    fix tplus.

--- themes/tplus/gulpfile.js
+++ themes/tplus/gulpfile.js
@@ -26,7 +26,7 @@ gulp.task('js', function() {
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
-        proxy: "http://yourdomain.com",
+        proxy: "http://color.server.host",
     });
 
     gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'scss/*.scss'], ['sass']);
