d53f68a5679d6a4b8ac16d746e0ae69888cd7698
John <John-lee@2001594.no-reply.drupal.org>
2018-09-01 21:08:01 +0800

    add page header.

--- themes/tplus/js/global.js
+++ themes/tplus/js/global.js
@@ -5,19 +5,20 @@
  */
 (function($, Drupal) {
 
-    'use strict';
+  'use strict';
 
-    Drupal.behaviors.bootstrap_barrio_subtheme = {
-        attach: function(context, settings) {
-            $(window).scroll(function() {
-                if ($(this).scrollTop() > 50) {
-                    $('body').addClass("scrolled");
-                } else {
-                    $('body').removeClass("scrolled");
-                }
-            });
-
+  Drupal.behaviors.bootstrap_barrio_subtheme = {
+    attach: function(context, settings) {
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > 50) {
+            $('body').addClass("scrolled");
+        } else {
+            $('body').removeClass("scrolled");
         }
-    };
+      });
+
+
+    }
+  };
 
 })(jQuery, Drupal);
