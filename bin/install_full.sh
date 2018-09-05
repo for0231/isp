0629cb5fe865aa45e51969099e895f88ef7244f6
John <John-lee@2001594.no-reply.drupal.org>
2018-09-05 21:16:54 +0800

    fix.

--- bin/install_full.sh
+++ bin/install_full.sh
@@ -2,9 +2,11 @@
 
 #composer require kgaut/potx
 #composer require drupal/drush_language
+composer require drupal/migrate_source_csv
+composer require drupal/migrate_tools
 vendor/bin/drush si -y --account-pass=admin --db-url=mysql://root:root@localhost/isp
 vendor/bin/drush en -y role_menu commerce_cart commerce_checkout commerce_payment_example commerce_recurring drush_language superfish
-vendor/bin/drush en -y config_update_ui
+vendor/bin/drush en -y config_update_ui migrate_source_csv
 vendor/bin/drupal thi coloradmin -y
 vendor/bin/drupal thi tplus -y
 vendor/bin/drush cset -y system.theme default tplus
