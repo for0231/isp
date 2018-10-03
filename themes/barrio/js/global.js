115a382ad77b6cf516e35c5620d49dc126ee50d9
John <John-lee@2001594.no-reply.drupal.org>
2018-10-03 21:44:30 +0800

    fix.

--- themes/barrio/js/global.js
+++ themes/barrio/js/global.js
@@ -3,21 +3,53 @@
  * Global utilities.
  *
  */
-(function($, Drupal) {
+(function ($, Drupal) {
 
-    'use strict';
+  'use strict';
 
-    Drupal.behaviors.bootstrap_barrio_subtheme = {
-        attach: function(context, settings) {
-            $(window).scroll(function() {
-                if ($(this).scrollTop() > 50) {
-                    $('body').addClass("scrolled");
-                } else {
-                    $('body').removeClass("scrolled");
-                }
-            });
+  Drupal.behaviors.bootstrap_barrio = {
+    attach: function (context, settings) {
 
-        }
-    };
+      var DELAY_NAV = 300;
+      var LOW_DELAY_NAV = 150;
+      var mainNavDebounce = debounce(DELAY_NAV);// 主导航菜单防抖
+      var $navLine = $('.header-nav-hover-line');
+      /* 主菜单“更多”的显示 */
+      var $header = $('#header');
+      var $nav = $header.find('.header-nav');
+      var $navItems = $nav.find('>ul>li');
+      var $navItems_notMore = $navItems.not('.header-nav-item-more');
+      var $navItemMore = $navItems.filter('.header-nav-item-more');
 
+      var setNavLine = {
+        active: function ($activeNavItem) {
+        },
+      };
+      $(context).find('.header-nav-list li').once('barrio_header-nav-list').hover(function() {
+        console.log($(this).width());
+      }, function() {
+
+      });
+
+
+
+
+
+    }
+  };
 })(jQuery, Drupal);
+/* 函数防抖 */
+var debounce = function (defaultTime) {
+  var timeoutIndex;
+  return function (callback, time) {
+    if (typeof time !== 'number') {
+      time = defaultTime;
+    }
+
+    if (typeof timeoutIndex === 'number') {
+      clearTimeout(timeoutIndex);
+    }
+
+    timeoutIndex = setTimeout(callback, time);
+  }
+};
