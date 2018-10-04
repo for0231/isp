8a9ed4bc0cb345cb72a8e9f8ab05440afcd4d677
John <John-lee@2001594.no-reply.drupal.org>
2018-10-04 16:11:20 +0800

    fix.

--- themes/barrio/js/global.js
+++ themes/barrio/js/global.js
@@ -7,52 +7,8 @@
 
   'use strict';
 
-  Drupal.behaviors.bootstrap_barrio = {
+  Drupal.behaviors.global= {
     attach: function (context, settings) {
-
-      var DELAY_NAV = 300;
-      var LOW_DELAY_NAV = 150;
-      var mainNavDebounce = debounce(DELAY_NAV);// 主导航菜单防抖
-      var $navLine = $('.header-nav-hover-line');
-      /* 主菜单“更多”的显示 */
-      var $header = $('#header');
-      var $nav = $header.find('.header-nav');
-      var $navItems = $nav.find('>ul>li');
-      var $navItems_notMore = $navItems.not('.header-nav-item-more');
-      var $navItemMore = $navItems.filter('.header-nav-item-more');
-
-      var setNavLine = {
-        active: function ($activeNavItem) {
-          if($activeNavItem.length === 0 || !$activeNavItem.is(':visible')) {
-              this.fold();
-              return
-          }
-          var left = $activeNavItem.position().left;
-          var width = $activeNavItem.width();
-
-          $activeNavItem.addClass('active').siblings().removeClass('active');
-
-          $navLine.css({
-              left: left,
-              width: width
-          });
-        },
-      };
-
-
-      $navItems.once('header_bottom').hover(function() {
-        var $this = $(this);
-        $this.addClass('hover');
-        $nav.addClass('item-hover');
-
-        setNavLine.active($this);
-      }, function() {
-        var $this = $(this);
-        console.log('fdasfdaf');
-      });
-
-
-
     }
   };
 })(jQuery, Drupal);
