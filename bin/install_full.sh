4260bb6bf69ea138c69dba5312d2be69170445f3
John <John-lee@2001594.no-reply.drupal.org>
2018-10-10 10:18:54 +0800

    fix.

--- bin/install_full.sh
+++ bin/install_full.sh
@@ -6,15 +6,15 @@
 #composer require drupal/migrate_tools
 vendor/bin/drush si -y --account-pass=admin --db-url=mysql://root:root@localhost/isp
 vendor/bin/drush en -y ds role_menu commerce_cart commerce_checkout commerce_payment_example commerce_recurring drush_language superfish
-vendor/bin/drush en -y libraries commerce_autosku views_slideshow
+vendor/bin/drush en -y libraries commerce_autosku views_slideshow views_slideshow_cycle default_content
 # Install views_slideshow jquery libraries
 vendor/bin/drush views:slideshow:lib
 vendor/bin/drush en -y config_update_ui migrate_source_csv block_class locale config_translation potx
-vendor/bin/drush en -y barrio_block isp_demodata
 vendor/bin/drupal thi coloradmin -y
 vendor/bin/drupal thi barrio -y
 vendor/bin/drush cset -y system.theme default barrio
 vendor/bin/drush cset -y system.theme admin coloradmin
 vendor/bin/drush language-add zh-hans
 vendor/bin/drush pmu -y toolbar
+vendor/bin/drush en -y barrio_block isp_demodata
 
