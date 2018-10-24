529c8188f3ae04fdcd1073a90e3c7f140f8341b8
John <John-lee@2001594.no-reply.drupal.org>
2018-10-24 12:43:38 +0800

    fix.

--- cbos/server/src/Plugin/Validation/Constraint/ServerUnique.php
+++ cbos/server/src/Plugin/Validation/Constraint/ServerUnique.php
@@ -2,7 +2,10 @@
 
 namespace Drupal\isp_server\Plugin\Validation\Constraint;
 
-use Drupal\Core\Validation\Plugin\Validation\Constraint\UniqueFieldConstraint;
+use Drupal\Core\Validation\Plugin\Validation\Constraint\UniqueFieldValueValidator;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidatorInterface;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 /**
  *
@@ -11,6 +14,35 @@ use Drupal\Core\Validation\Plugin\Validation\Constraint\UniqueFieldConstraint;
  *   label = @Translation("Server unique", context = "Validation")
  * )
  */
-class ServerUnique extends UniqueFieldConstraint {
+class ServerUnique extends Constraint implements ConstraintValidatorInterface {
+  /** @var \Symfony\Component\Validator\Context\ExecutionContextInterface $context */
+  protected $context;
+
   public $message = 'The server %value is already taken.';
+
+  public function validate($items, Constraint $constraint) {
+    if (!$items->first()) {
+      return;
+    }
+    $options = [];
+    foreach ($items->referencedEntities() as $item) {
+      $options[$item->id()] = $item->label();
+    }
+    if ($items->count() != count($options)) {
+      $this->context->addViolation('The server someone is already taken.');
+    }
+  }
+
+  public function validatedBy() {
+    return get_class($this);
+  }
+
+  /**
+   * Initializes the constraint validator.
+   *
+   * @param ExecutionContextInterface $context The current validation context
+   */
+  public function initialize(ExecutionContextInterface $context) {
+    $this->context = $context;
+  }
 }
