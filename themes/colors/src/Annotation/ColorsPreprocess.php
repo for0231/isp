d69a62d04837d944f4d67317cbc546a6ff83c97b
John <John@139.com>
2018-12-25 21:41:45 +0800

    fix.

---
+++ themes/colors/src/Annotation/ColorsPreprocess.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\colors\Annotation;
+
+/**
+ * Defines a ColorsPreprocess annotation object.
+ *
+ * Plugin Namespace: "Plugin/Preprocess"
+ * @see \Drupal\colors\Plugin\Preprocess\PreprocessInterface
+ * @see \Drupal\colors\Plugin\Preprocess\PreprocessManager
+ * @see plugin_api
+ *
+ * @Annotation
+ *
+ * @ingroup plugins_preprocess
+ */
+class ColorsPreprocess extends PluginCallback {}
---
+++ themes/colors/src/Colors.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\colors;
+
+/**
+ * Thre primary class for the Drupal Colors base theme.
+ *
+ * Provides many helper methods.
+ *
+ * @ingroup utility
+ */
+class Colors {
+  
+  /**
+   * Append a callback.
+   *
+   * @var int
+   */
+  const CALLBACK_APPEND = 1;
+}
