04e213dde1c0068af66ffd6b2c4755b7bcfc10cc
John <John@139.com>
2018-12-29 17:55:48 +0800

    fix.

---
+++ themes/smart/assets/js/extension.js
@@ -0,0 +1,7 @@
+(function ($, Drupal, drupalSettings) {
+  Drupal.behaviors.extension = {
+    attach: function attach(context) {
+      console.log('afdaf');
+    }
+  };
+})(jQuery, Drupal, drupalSettings);
