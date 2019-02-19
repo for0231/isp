a0e00b8c723b4a72c6a80f02a1899eda2b50ef6f
John <lijd@eabax.com>
2019-02-19 21:37:32 +0800

    fix simple test.

--- bin/simpletest.sh
+++ bin/simpletest.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
 ignores=( \
-  'account' \
-  'activity' \
-  'activity_cost'\
-  'calendar' \
   'eabax_seven' \
 )
 
-PROJECT="modules/isp"
+PROJECTS=(
+  "modules/eabax" \
+  "modules/isp" \
+)
 OUTPUT="simpletest-`date +%Y%m%d`.txt"
-NUMS=`find ${PROJECT} -name "*.info.yml" |wc -l`
-echo ${NUMS}
-rm $OUTPUT
-for file in `find ${PROJECT} -name "*.info.yml"`; do
-  FOUND=0
-  for module in ${ignores[@]}; do
-    if [[ $(basename $(dirname ${file})) == ${module} ]]; then
-      FOUND=1
-      break;
+#NUMS=`find ${PROJECT} -name "*.info.yml" |wc -l`
+#echo ${NUMS}
+rm -f ${OUTPUT}
+for PROJECT in ${PROJECTS[@]}; do
+  for file in `find ${PROJECT} -name "*.info.yml"`; do
+    FOUND=0
+    for module in ${ignores[@]}; do
+      if [[ $(basename $(dirname ${file})) == ${module} ]]; then
+        FOUND=1
+        break;
+      fi
+    done
+    if [[ ${FOUND} == 1 ]]; then
+      echo 'ignore' - $(basename $(dirname ${file}))
+    else
+      sudo -u $USER php \
+        ./core/scripts/run-tests.sh --url http://localhost --verbose \
+      $(basename $(dirname ${file}))# >> $OUTPUT
     fi
   done
-  if [[ ${FOUND} == 1 ]]; then
-    echo 'ignore' - $(basename $(dirname ${file}))
-  else
-    sudo -u $USER php \
-      ./core/scripts/run-tests.sh --url http://localhost --verbose \
-    $(basename $(dirname ${file})) >> $OUTPUT
-  fi
 done
