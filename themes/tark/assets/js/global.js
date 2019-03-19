1c9bce7518c435578fe758155ac60b87db8a3749
John <lijd@eabax.com>
2019-03-19 14:29:15 +0800

    fix.

--- themes/tark/assets/js/global.js
+++ themes/tark/assets/js/global.js
@@ -9,6 +9,12 @@
   Drupal.behaviors.tark = {
     attach: function (context, settings) {
       $('#block-rolemenublock').metisMenu();
+
+      $('<table class="sticky-header"/>').onLoad(function () {
+        $(this).css({
+          top: '48px',
+        });
+      });
     }
   };
 }) (jQuery, Drupal, drupalSettings);
